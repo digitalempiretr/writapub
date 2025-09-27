afb96fca7f51b42e0707d7a69319a405859effb0
Mirac Özel <digitalartsale@gmail.com>
2025-09-27 10:53:59 +0000

    Runtime Error
    Error: Invalid src prop (https://images.pexels.com/photos

--- next.config.ts
+++ next.config.ts
@@ -23,8 +23,14 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https,"
         hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
         port: '',
         pathname: '/**',
       },
