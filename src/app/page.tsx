1ef40b23c6a7c76543aa3a99bc81af5fb7e4d151
Mirac Özel <digitalartsale@gmail.com>
2025-09-29 09:25:19 +0000

    Güzel. Bu Hazır görsel şablon karuselinin altında hazır arama butonlarıı

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,6 +43,11 @@ import { CardTitle } from "@/components/ui/card";
 import Lottie from 'lottie-react';
 import webflowAnimation from '@/lib/Lottiefiles + Webflow.json';
 import { imageTemplates } from "@/lib/image-templates";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 type Design = {
   text: string;
@@ -504,12 +509,15 @@ export default function Home() {
                                 <CarouselNext className="-right-4" />
                               </Carousel>
                               
-                              <div className="flex flex-wrap gap-2">
-                                {searchKeywords.map(keyword => (
-                                    <Button key={keyword} variant="outline" size="sm" onClick={() => handleKeywordSearch(keyword)}>
-                                        {keyword}
-                                    </Button>
-                                ))}
+                              <div className="space-y-2">
+                                <Label>İlham Veren Temalar</Label>
+                                <div className="flex gap-2">
+                                  {searchKeywords.map(keyword => (
+                                      <Button key={keyword} variant="outline" size="sm" onClick={() => handleKeywordSearch(keyword)} className="flex-1">
+                                          {keyword}
+                                      </Button>
+                                  ))}
+                                </div>
                               </div>
 
                               <div className="flex items-center space-x-2">
@@ -552,8 +560,8 @@ export default function Home() {
                       </TabsContent>
                       <TabsContent value="text">
                         <div className="p-4 bg-[#f4fdff] text-card-foreground rounded-b-lg space-y-4">
-                           <div className="grid grid-cols-[auto_auto_1fr_auto] gap-x-4 gap-y-4 items-center">
-                            {/* Row 1: Text Color & Font */}
+                           <div className="grid grid-cols-[auto_auto_1fr_auto_auto] gap-x-2 items-center">
+                            {/* Row 1: Text Color, Font, Alignment */}
                             <div className="flex items-center justify-center">
                               <Baseline className="h-5 w-5" />
                             </div>
@@ -607,31 +615,38 @@ export default function Home() {
                               </DropdownMenuContent>
                             </DropdownMenu>
 
-                            {/* Row 2: Text Box Background */}
-                            <div className="col-span-4 flex items-center gap-2">
-                              <Brush className="h-5 w-5" />
-                              <div className="relative">
-                                <div
-                                  className="w-6 h-6 rounded-full border"
-                                  style={{ backgroundColor: rectBgColor }}
-                                />
-                                <Input
-                                  type="color"
-                                  value={rectBgColor}
-                                  onChange={(e) => setRectBgColor(e.target.value)}
-                                  className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
-                                />
-                              </div>
-                              <Slider
-                                max={1}
-                                min={0}
-                                step={0.05}
-                                value={[rectOpacity]}
-                                onValueChange={(value) => setRectOpacity(value[0])}
-                                style={{'--slider-track-bg': rectBgColor} as React.CSSProperties}
-                                className="flex-grow [&>span:first-child]:bg-[var(--slider-track-bg)]"
-                              />
-                            </div>
+                             <Popover>
+                                <PopoverTrigger asChild>
+                                    <Button variant="outline" size="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className="fill-current"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h200v720H200Zm280 0q-17 0-28.5-11.5T440-160q0-17 11.5-28.5T480-200q17 0 28.5 11.5T520-160q0 17-11.5 28.5T480-120Zm0-160q-17 0-28.5-11.5T440-320q0-17 11.5-28.5T480-360q17 0 28.5 11.5T520-320q0 17-11.5 28.5T480-280Zm0-160q-17 0-28.5-11.5T440-480q0-17 11.5-28.5T480-520q17 0 28.5 11.5T520-480q0 17-11.5 28.5T480-440Zm0-160q-17 0-28.5-11.5T440-640q0-17 11.5-28.5T480-680q17 0 28.5 11.5T520-640q0 17-11.5 28.5T480-600Zm0-160q-17 0-28.5-11.5T440-800q0-17 11.5-28.5T480-840q17 0 28.5 11.5T520-800q0 17-11.5 28.5T480-760Zm80 560q-17 0-28.5-11.5T520-240q0-17 11.5-28.5T560-280q17 0 28.5 11.5T600-240q0 17-11.5 28.5T560-200Zm0-160q-17 0-28.5-11.5T520-400q0-17 11.5-28.5T560-440q17 0 28.5 11.5T600-400q0 17-11.5 28.5T560-360Zm0-160q-17 0-28.5-11.5T520-560q0-17 11.5-28.5T560-600q17 0 28.5 11.5T600-560q0 17-11.5 28.5T560-520Zm0-160q-17 0-28.5-11.5T520-720q0-17 11.5-28.5T560-760q17 0 28.5 11.5T600-720q0 17-11.5 28.5T560-680Zm80 560q-17 0-28.5-11.5T600-160q0-17 11.5-28.5T640-200q17 0 28.5 11.5T680-160q0 17-11.5 28.5T640-120Zm0-160q-17 0-28.5-11.5T600-320q0-17 11.5-28.5T640-360q17 0 28.5 11.5T680-320q0 17-11.5 28.5T640-280Zm0-160q-17 0-28.5-11.5T600-480q0-17 11.5-28.5T640-520q17 0 28.5 11.5T680-480q0 17-11.5 28.5T640-440Zm0-160q-17 0-28.5-11.5T600-640q0-17 11.5-28.5T640-680q17 0 28.5 11.5T680-640q0 17-11.5 28.5T640-600Zm0-160q-17 0-28.5-11.5T600-800q0-17 11.5-28.5T640-840q17 0 28.5 11.5T680-800q0 17-11.5 28.5T640-760Zm80 560q-17 0-28.5-11.5T680-240q0-17 11.5-28.5T720-280q17 0 28.5 11.5T760-240q0 17-11.5 28.5T720-200Zm0-160q-17 0-28.5-11.5T680-400q0-17 11.5-28.5T720-440q17 0 28.5 11.5T760-400q0 17-11.5 28.5T720-360Zm0-160q-17 0-28.5-11.5T680-560q0-17 11.5-28.5T720-600q17 0 28.5 11.5T760-560q0 17-11.5 28.5T720-520Zm0-160q-17 0-28.5-11.5T680-720q0-17 11.5-28.5T720-760q17 0 28.5 11.5T760-720q0 17-11.5 28.5T720-680Zm80 560q-17 0-28.5-11.5T760-160q0-17 11.5-28.5T800-200q17 0 28.5 11.5T840-160q0 17-11.5 28.5T800-120Zm0-160q-17 0-28.5-11.5T760-320q0-17 11.5-28.5T800-360q17 0 28.5 11.5T840-320q0 17-11.5 28.5T800-280Zm0-160q-17 0-28.5-11.5T760-480q0-17 11.5-28.5T800-520q17 0 28.5 11.5T840-480q0 17-11.5 28.5T800-440Zm0-160q-17 0-28.5-11.5T760-640q0-17 11.5-28.5T800-680q17 0 28.5 11.5T840-640q0 17-11.5 28.5T800-600Zm0-160q-17 0-28.5-11.5T760-800q0-17 11.5-28.5T800-840q17 0 28.5 11.5T840-800q0 17-11.5 28.5T800-760Z"/></svg>
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-full space-y-4">
+                                     <div className="flex items-center gap-2">
+                                        <div className="relative">
+                                            <div
+                                            className="w-6 h-6 rounded-full border"
+                                            style={{ backgroundColor: rectBgColor }}
+                                            />
+                                            <Input
+                                            type="color"
+                                            value={rectBgColor}
+                                            onChange={(e) => setRectBgColor(e.target.value)}
+                                            className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                                            />
+                                        </div>
+                                        <Slider
+                                            max={1}
+                                            min={0}
+                                            step={0.05}
+                                            value={[rectOpacity]}
+                                            onValueChange={(value) => setRectOpacity(value[0])}
+                                            style={{'--slider-track-bg': rectBgColor} as React.CSSProperties}
+                                            className="flex-grow [&>span:first-child]:bg-[var(--slider-track-bg)]"
+                                        />
+                                    </div>
+                                </PopoverContent>
+                            </Popover>
                           </div>
                         </div>
                       </TabsContent>
