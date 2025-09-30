6fee9ac50482911439863c2a1e0fdff49a44c90b
Mirac Özel <digitalartsale@gmail.com>
2025-09-30 11:05:29 +0000

    bu ikonu kullan <svg xmlns="http://www.w3.org/2000/svg" height="40px" vi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -600,7 +600,7 @@ export default function Home() {
                                     <Tooltip>
                                       <TooltipTrigger asChild>
                                           <div className="relative">
-                                              <svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 0 24 24" width="28px" fill={overlayColor}><path d="M0 0h24v24H0V0z" fill="none"/><path d="M7 17h14V3H7v14zm4.25-5.53l1.96 2.36 2.75-3.54L19.5 15h-11l2.75-3.53z" opacity=".3"/><path d="M1 21c0 1.1.9 2 2 2h16v-2H3V5H1v16zM21 1H7c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V3c0-1.1-.9-2-2-2zm0 16H7V3h14v14zm-5.04-6.71l-2.75 3.54-1.96-2.36L8.5 15h11z"/></svg>
+                                              <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill={overlayColor}><path d="M0 0h24v24H0V0z" fill="none"/><path d="M7 17h14V3H7v14zm4.25-5.53l1.96 2.36 2.75-3.54L19.5 15h-11l2.75-3.53z" opacity=".3"/><path d="M1 21c0 1.1.9 2 2 2h16v-2H3V5H1v16zM21 1H7c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V3c0-1.1-.9-2-2-2zm0 16H7V3h14v14zm-5.04-6.71l-2.75 3.54-1.96-2.36L8.5 15h11z"/></svg>
                                               <Input
                                               type="color"
                                               value={overlayColor}
@@ -774,7 +774,7 @@ export default function Home() {
                                         min={0}
                                         step={0.01}
                                         value={[rectOpacity]}
-                                        onChange={(value) => setRectOpacity(value[0])}
+                                        onValueChange={(value) => setRectOpacity(value[0])}
                                         className="flex-grow"
                                       />
                                       <div className="text-sm p-2 rounded-md border border-input tabular-nums w-14 text-center">
