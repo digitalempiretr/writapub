7d9a717b5c0add5dbfca593db385906181ebcef7
Mirac Özel <digitalartsale@gmail.com>
2025-09-30 11:08:15 +0000

    ikonlar için bu kodları kullanmak için ne yapmak lazım google material c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -600,13 +600,13 @@ export default function Home() {
                                     <Tooltip>
                                       <TooltipTrigger asChild>
                                           <div className="relative">
-                                              <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill={overlayColor}><path d="M0 0h24v24H0V0z" fill="none"/><path d="M7 17h14V3H7v14zm4.25-5.53l1.96 2.36 2.75-3.54L19.5 15h-11l2.75-3.53z" opacity=".3"/><path d="M1 21c0 1.1.9 2 2 2h16v-2H3V5H1v16zM21 1H7c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V3c0-1.1-.9-2-2-2zm0 16H7V3h14v14zm-5.04-6.71l-2.75 3.54-1.96-2.36L8.5 15h11z"/></svg>
-                                              <Input
-                                              type="color"
-                                              value={overlayColor}
-                                              onChange={(e) => setOverlayColor(e.target.value)}
-                                              className="absolute inset-0 h-full w-full cursor-pointer opacity-0"
-                                              />
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill={overlayColor}><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.56 10.81l-2.35 3.02-1.56-1.88c-.2-.25-.58-.24-.78.01l-1.74 2.23c-.26.33-.02.81.39.81h8.98c.41 0 .65-.47.4-.8l-2.55-3.39c-.19-.26-.59-.26-.79 0zM2 5c-.55 0-1 .45-1 1v15c0 1.1.9 2 2 2h15c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1-.45-1-1V6c0-.55-.45-1-1-1zm19-4H7c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V3c0-1.1-.9-2-2-2zm-1 16H8c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h12c.55 0 1 .45 1 1v12c0 .55-.45 1-1 1z"/></svg>
+                                            <Input
+                                            type="color"
+                                            value={overlayColor}
+                                            onChange={(e) => setOverlayColor(e.target.value)}
+                                            className="absolute inset-0 h-full w-full cursor-pointer opacity-0"
+                                            />
                                           </div>
                                       </TooltipTrigger>
                                       <TooltipContent>
@@ -619,7 +619,7 @@ export default function Home() {
                                       <TooltipTrigger asChild>
                                         <PopoverTrigger asChild>
                                           <Button variant="outline" size="icon">
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#3F87A5"><path d="M0 0h24v24H0z" fill="none"/><path d="M11 9h2v2h-2V9zm-2 2h2v2H9v-2zm4 0h2v2h-2v-2zm2-2h2v2h-2V9zM7 9h2v2H7V9zm12-6H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 18H7v-2h2v2zm4 0h-2v-2h2v2zm4 0h-2v-2h2v2zm2-7h-2v2h2v2h-2v-2h-2v2h-2v-2h-2v2H9v-2H7v2H5v-2h2v-2H5V6c0-.55.45-1 1-1h12c.55 0 1 .45 1 1v5z"/></svg>
+                                            <i className="material-icons" style={{fontSize: '30px', color: '#3F87A5'}}>view_quilt</i>
                                           </Button>
                                         </PopoverTrigger>
                                       </TooltipTrigger>
