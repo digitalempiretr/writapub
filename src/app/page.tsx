a4a7593c82706b08774fd0b56fd1a4fd9d8a042c
Mirac Özel <digitalartsale@gmail.com>
2025-10-07 08:41:34 +0000

    bu karuselde tek seferde 4 tasarım görünsün. 4 tane sığması için width d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -194,7 +194,7 @@ function TabContentContainer({
           >
             <CarouselContent className="-ml-2">
               {designTemplates.map((template) => (
-                <CarouselItem key={template.name} className="basis-1/2 md:basis-1/3 pl-2">
+                <CarouselItem key={template.name} className="basis-1/2 md:basis-1/4 pl-2">
                   <button onClick={() => handleApplyTemplate(template)} className="w-full group">
                     <Card className="overflow-hidden">
                       <CardContent className="p-0">
@@ -238,7 +238,7 @@ function TabContentContainer({
             >
               <CarouselContent className="-ml-2">
                 {myDesigns.map((template) => (
-                  <CarouselItem key={template.id} className="basis-1/2 md:basis-1/3 pl-2">
+                  <CarouselItem key={template.id} className="basis-1/2 md:basis-1/4 pl-2">
                     <div className="relative group">
                        <button onClick={() => editingDesignId !== template.id && handleApplyTemplate(template)} className="w-full" disabled={editingDesignId === template.id}>
                         <Card className="overflow-hidden">
@@ -1122,7 +1122,7 @@ export default function Home() {
             imageUrl = undefined;
             break;
         case "image":
-            currentBg = undefined;
+            currentBg = imageBgUrl;
             imageUrl = imageBgUrl;
             break;
         default:
@@ -1471,7 +1471,5 @@ export default function Home() {
     </>
   );
 }
-
-    
 
     
