7cae96722b6900f493c14af284d44c2ee8ccaf7c
Mirac Özel <digitalartsale@gmail.com>
2025-11-11 02:07:01 +0000

    bunu w-100 yapıyorum. ısrarla benim yaptığım değişiklikleri bozuyorsun.

--- src/components/3_text-settings.tsx
+++ src/components/3_text-settings.tsx
@@ -217,19 +217,19 @@ export function TextSettings({
   const unitOptions: Unit[] = ['px', 'em', 'rem'];
 
   const fontSizeContent = (
-    <div className="w-80 space-y-4">
+    <div className="w-full space-y-4">
       <div className="space-y-2">
           <Label htmlFor={`${baseId}-font-size-slider`}>Font Size</Label>
            <div className="flex items-center gap-2">
               <Slider id={`${baseId}-font-size-slider`} max={200} min={16} step={1} value={[internalFontSize]} onValueChange={(v) => setInternalFontSize(v[0])} onValueCommit={(v) => setFontSize(v[0])} className="flex-grow"/>
-              <Input type="number" value={internalFontSize} onChange={(e) => setInternalFontSize(Number(e.target.value))} onBlur={() => setFontSize(internalFontSize)} className="h-8 text-xs flex-grow" />
+              <Input type="number" value={internalFontSize} onChange={(e) => setInternalFontSize(Number(e.target.value))} onBlur={() => setFontSize(internalFontSize)} className="h-8 text-xs" />
            </div>
       </div>
       <div className="space-y-2">
           <Label htmlFor={`${baseId}-line-height-slider`}>Line Height</Label>
           <div className="flex items-center gap-2">
               <Slider id={`${baseId}-line-height-slider`} max={2.5} min={0.8} step={0.1} value={[internalLineHeight]} onValueChange={(v) => setInternalLineHeight(v[0])} onValueCommit={(v) => handleLineHeightChange(v[0])} className="flex-grow"/>
-               <Input type="number" value={internalLineHeight} onChange={(e) => setInternalLineHeight(Number(e.target.value))} onBlur={() => handleLineHeightChange(internalLineHeight)} step="0.1" className="h-8 text-xs flex-grow" />
+               <Input type="number" value={internalLineHeight} onChange={(e) => setInternalLineHeight(Number(e.target.value))} onBlur={() => handleLineHeightChange(internalLineHeight)} step="0.1" className="h-8 text-xs" />
           </div>
       </div>
     </div>
@@ -509,7 +509,7 @@ export function TextSettings({
                       </TooltipTrigger>
                       <TooltipContent><p>Font Size & Line Height</p></TooltipContent>
                     </Tooltip>
-                    <PopoverContent className="w-80 space-y-4">
+                    <PopoverContent className="w-full space-y-4">
                       {fontSizeContent}
                     </PopoverContent>
                   </Popover>
